83e481c4a05983d854284cccf0dad6d2008c0735
svoboda18 <dev.svoboda18@gmail.com>
2024-02-03 16:54:32 +0100

    feat(docker): add image to deploy the frontend
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Create index and index old articles
-yes | python manage.py search_index --rebuild
+if [ "$1" == "backend" ]; then
+    # Create index and index old articles
+    yes | python manage.py search_index --rebuild
 
-# Migrate to apply any new db update
-python manage.py makemigrations article search_indexes user
-python manage.py migrate
+    # Migrate to apply any new db update
+    python manage.py makemigrations article search_indexes user
+    python manage.py migrate
 
-# Start the server
-python manage.py runserver 0.0.0.0:8000 &
+    # Start the server
+    python manage.py runserver 0.0.0.0:8000 &
+else # frontend
+    # make sure the image has latest source
+    yes | git pull
+    # Start the server (dev)
+    npm run dev
+fi
 
-exec "$@"
+exec "${@:2}"
 
 sleep infinity

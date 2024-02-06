0f8e57525f7de11c506e981ea35d222245871de4
Yahia Abdeldjalil <ly_benyahia@esi.dz>
2024-02-06 23:00:41 +0100

    Added chars modified counter (models, views, urls)

--- article/models.py
+++ article/models.py
@@ -1,35 +1,49 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 
+
 class Author(models.Model):
     name = models.CharField(blank=False, max_length=255)
+
 
 class Institution(models.Model):
     name = models.CharField(blank=False, max_length=255)
 
+
 class Keyword(models.Model):
     name = models.CharField(blank=False, max_length=64)
 
+
 class Refrence(models.Model):
     name = models.CharField(blank=False, max_length=255)
+
+
+# Characters modified counter
+class CharCount(models.Model):
+    count = models.IntegerField(default=0)
+
 
 class Article(models.Model):
     title = models.CharField(blank=False, max_length=255)
     body = models.TextField(blank=False)
     resume = models.TextField(blank=False)
-    pdf = models.FileField(blank=False, validators=[FileExtensionValidator(allowed_extensions=['pdf'])], upload_to='uploaded_articles')
+    pdf = models.FileField(
+        blank=False,
+        validators=[FileExtensionValidator(allowed_extensions=["pdf"])],
+        upload_to="uploaded_articles",
+    )
 
-    authors = models.ManyToManyField(Author, related_name='articles')
-    institutions = models.ManyToManyField(Institution, related_name='articles')
-    keywords = models.ManyToManyField(Keyword, related_name='articles')
-    refrences = models.ManyToManyField(Refrence, related_name='articles')
+    authors = models.ManyToManyField(Author, related_name="articles")
+    institutions = models.ManyToManyField(Institution, related_name="articles")
+    keywords = models.ManyToManyField(Keyword, related_name="articles")
+    refrences = models.ManyToManyField(Refrence, related_name="articles")
 
-    created_at = models.DateTimeField(auto_now_add=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
         """Meta options."""
 
